58b07c1000fceacb31e308b70cdbcc0cedc92725
Haseeb Arshad <haseeb9009@gmail.com>
2023-08-04 13:15:45 -0600

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+aws ecr get-login-password --region ap-southeast-1 | sudo docker login --username AWS --password-stdin 684904726815.dkr.ecr.ap-southeast-1.amazonaws.com
 sed -i "s/TAG=.*/TAG=$1/g" .env
 sudo docker-compose run --rm app bin/rake db:migrate
 sudo docker-compose up -d
